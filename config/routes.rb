42a4535c9c42d11076255e4e8ebd0588fce5f3e2
Rakesh Pankhania <rakesh@act.md>
2017-06-03 21:46:53 -0400

    Root URL redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get root to: 'routes#index'
+
   get 'routes/:id', to: redirect('/routes/%{id}/outbound')
   get 'routes/:id/:direction', to:'routes#show'
+
   resources :routes, only: [:index, :show]
 end
