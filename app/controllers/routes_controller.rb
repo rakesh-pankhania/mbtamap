c2ecd04b70264f5a29c11dcf8d9c88c04c0d1379
Rakesh Pankhania <rakesh@act.md>
2018-09-15 15:02:47 -0400

    Working with static vehicles

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -8,73 +8,33 @@ class RoutesController < ApplicationController
   end
 
   def show
-    # TODO: predictionsbyroute returns alert headers, display them?
+    direction_id = Trip::DIRECTIONS.find_index(route_params[:direction])
 
-    direction_name = route_params[:direction]
-    direction_id = Trip::DIRECTIONS.find_index(direction_name)
+    trip_ids = Trip.where(
+      route_external_id: @route.external_id,
+      direction_id: direction_id
+    ).pluck(:external_id)
 
-    # Get route predictions
-    prediction_json = JSON.parse(
+    @shapes = Shape.joins(:trips)
+                   .where(trips: { external_id: trip_ids })
+                   .distinct
+
+    @stops = Stop.joins(:stop_times)
+                 .where(stop_times: { trip_external_id: trip_ids })
+                 .distinct
+
+    @vehicles = JSON.parse(
       Net::HTTP.get(
-        URI::HTTP.build(
-          host: "realtime.mbta.com",
-          path: "/developer/api/v2/predictionsbyroute",
+        URI::HTTPS.build(
+          host: "api-v3.mbta.com",
+          path: "/vehicles",
           query: {
-            api_key: ENV['MBTA_API_KEY'],
-            route: @route.external_id,
-            direction: direction_id,
-            format: 'json'
+            'filter[route]': @route.external_id,
+            'filter[direction_id]': direction_id
           }.to_query
         )
       )
-    )
-
-    @stops = {}.with_indifferent_access
-    @trip_ids = Set.new
-    @stop_ids = Set.new
-    trips = prediction_json['direction'][direction_id]['trip']
-    trips.each do |trip|
-      @trip_ids << trip['trip_id']
-      trip['stop'].each do |stop|
-        @stop_ids << stop['stop_id']
-        sequence = stop['stop_sequence'].to_i
-        @stops[sequence] ||= { name: stop['stop_name'] }
-        @stops[sequence]['times'] ||= []
-        time = nil
-        if stop['sch_arr_dt'].present?
-          time = Time.at(stop['sch_arr_dt'].to_i)
-        elsif stop['pre_dt'].present?
-          time = Time.at(stop['pre_dt'].to_i)
-        end
-        unless time.nil?
-          time = time.strftime('%l:%M %P')
-          time = "#{time} (#{trip['trip_headsign']})" unless trip['trip_headsign'].nil?
-          @stops[sequence]['times'] << time
-        end
-      end
-    end
-
-    # Get realtime locations
-    realtime_json = JSON.parse(
-      Net::HTTP.get(
-        URI::HTTP.build(
-          host: "realtime.mbta.com",
-          path: "/developer/api/v2/vehiclesbyroute",
-          query: {
-            api_key: ENV['MBTA_API_KEY'],
-            route: @route.external_id,
-            format: 'json'
-          }.to_query
-        )
-      )
-    )
-    @vehicles = []
-    realtime_json["direction"][direction_id]["trip"].each do |trip|
-      @vehicles << trip['vehicle']
-    end
-
-    @stop_objects = Stop.where(external_id: @stop_ids.to_a)
-    @shapes = Shape.where(id: Trip.where(external_id: @trip_ids.to_a).select(:shape_id))
+    )['data']
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get root to: 'routes#index'
 
   get 'routes/:id', to: redirect('/routes/%{id}/outbound')
-  get 'routes/:id/:direction', to:'routes#show'
+  get 'routes/:id/:direction', to:'routes#show', constraints: { direction: /inbound|outbound/}
 
   resources :routes, only: [:index, :show]
 end
