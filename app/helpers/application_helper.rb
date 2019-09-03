37265c1b0cf4b4621739596ad328ec930abb44a9
Rakesh Pankhania <rakesh@act.md>
2019-09-03 19:56:38 -0400

    Start mobile friendliness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def direction_button_style(direction)
-    @direction == direction ? 'btn btn-secondary' : 'btn btn-outline-secondary'
+    @direction == direction ? 'link-active' : 'link-inactive'
   end
 end
