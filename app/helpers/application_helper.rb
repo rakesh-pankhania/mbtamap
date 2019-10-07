15a780d1dd945ac95fe45e9c5b8a4a2c3716199e
Rakesh Pankhania <rakesh@act.md>
2019-10-07 08:21:18 -0400

    Add more styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def direction_button_style(direction)
-    @direction == direction ? 'button' : 'button hollow'
+    @direction == direction ? 'tabs-title text-center is-active' : 'tabs-title text-center'
   end
 end
